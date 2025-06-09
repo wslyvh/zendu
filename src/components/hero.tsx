7a20d744040c57880b6b5f2a84935b00acb3ab59
wslyvh <wslyvh@users.noreply.github.com>
2025-06-09 14:17:20 +0200

    mobile spacing

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -42,67 +42,69 @@ export function Hero() {
           alt="Zendu meditating robot with bonsai and hourglass"
           width={1020}
           height={600}
-          className="w-full max-w-xl h-auto mb-6"
+          className="w-full max-w-xs md:max-w-xl h-auto mb-2 md:mb-6"
           priority
         />
 
-        <div className="mb-4">
-          <span className="badge badge-accent badge-outline rounded-full">
+        <div className="mb-2 md:mb-4">
+          <span className="badge badge-accent badge-outline rounded-full badge-sm md:badge-md">
             ✨ Human-powered Accountability Coach
           </span>
         </div>
 
-        <h1 className="text-4xl md:text-5xl  text-gray-800 font-extrabold mb-4 leading-tight">
+        <h1 className="text-2xl md:text-5xl text-gray-800 font-extrabold mb-2 md:mb-4 leading-tight">
           Build momentum,{" "}
           <span className="bg-gradient-to-r from-orange-600 to-orange-400 bg-clip-text text-transparent">
             not pressure
           </span>
         </h1>
 
-        <p className="text-lg md:text-xl text-gray-600 mb-8">
+        <p className="text-base md:text-xl text-gray-600">
           Zendu helps solo founders and indie hackers cultivate consistency,
-          focus, and momentum through gentle guidance. Daily progress, without
-          the pressure.
+          focus, and momentum through gentle guidance.
+        </p>
+        <p className="text-base md:text-xl text-gray-600 mb-2 md:mb-8">
+          Daily progress, with Zendu.
         </p>
 
         {/* CTA */}
-        <div className="flex flex-col md:flex-row justify-center items-center gap-4 w-full mb-6">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-2 md:gap-4 w-full mb-4 md:mb-6">
           <Link
             href="https://farcaster.xyz/~/inbox/create/12580?text=I want to stay focused and start a 5-day momentum sprint. Let's go 🚀"
             target="_blank"
             rel="noopener noreferrer"
-            className="btn btn-primary rounded-full w-full md:w-auto"
+            className="btn btn-primary rounded-full w-full md:btn-wide"
           >
-            Try a 5-day momentum sprint →
+            Try a 5-day sprint →
           </Link>
           {subscribeButton()}
         </div>
 
-        <div className="flex flex-wrap justify-center gap-6 text-base-content/60 font-medium">
+        <div className="flex flex-wrap justify-center gap-2 md:gap-6 text-base-content/60 font-medium">
           <div className="flex items-center gap-2">
-            <div className="avatar-group -space-x-6">
+            <div className="avatar-group -space-x-4 md:-space-x-6">
               <div className="avatar">
-                <div className="w-8">
+                <div className="w-6 md:w-10">
                   <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
                 </div>
               </div>
               <div className="avatar">
-                <div className="w-8">
+                <div className="w-6 md:w-10">
                   <img src="https://img.daisyui.com/images/profile/demo/spiderperson@192.webp" />
                 </div>
               </div>
               <div className="avatar">
-                <div className="w-8">
+                <div className="w-6 md:w-10">
                   <img src="https://img.daisyui.com/images/profile/demo/averagebulk@192.webp" />
                 </div>
               </div>
               <div className="avatar avatar-placeholder">
-                <div className="bg-neutral text-neutral-content w-8 font-normal text-xs">
+                <div className="bg-neutral text-neutral-content w-6 md:w-10 font-normal text-xs">
                   <span>+10</span>
                 </div>
               </div>
             </div>
-            are building momentum
+            <span className="text-xs md:text-base">are building momentum</span>
           </div>
         </div>
       </div>
--- src/components/newsletter.tsx
+++ src/components/newsletter.tsx
@@ -1,30 +1,60 @@
 export function Newsletter() {
   return (
     <section
-      className="hero bg-base-200 rounded-2xl p-4 text-gray-800 container mx-auto"
+      className="hero bg-base-200 rounded-2xl p-4 text-gray-800 container mx"
       id="subscribe"
     >
       <div className="flex flex-col gap-4 items-center hero-content text-center">
-        <h3 className="text-2xl font-bold">Stay up to date with Zendu</h3>
+        <h3 className="text-2xl font-bold">Stay up to date</h3>
         <p className="text-gray-600 mb-4">
           Subscribe to be the first to know when Zendu launches.
         </p>
         <form
-          className="flex flex-col gap-4 md:flex-row"
+          className=" join"
           target="_blank"
           method="post"
           action="https://login.sendpulse.com/forms/simple/u/eyJ1c2VyX2lkIjo4Mzk2NTA3LCJhZGRyZXNzX2Jvb2tfaWQiOjEwNjE3OTQsImxhbmciOiJlbiJ9"
         >
-          <input
-            className="input input-neutral w-full rounded-full"
-            name="email"
-            type="email"
-            placeholder="Enter your email.."
-            required
-          />
-          <input type="hidden" name="sender" value="aGVsbG9Ad2VzLmVtYWls" />
-          <input type="hidden" name="name" value="" />
-          <button className="btn btn-neutral rounded-full">Subscribe</button>
+          <div className="join">
+            <div>
+              <label className="input validator join-item rounded-l-full">
+                <svg
+                  className="h-[1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                  </g>
+                </svg>
+                <input
+                  name="email"
+                  type="email"
+                  placeholder="mail@site.com"
+                  required
+                />
+                <input
+                  type="hidden"
+                  name="sender"
+                  value="aGVsbG9Ad2VzLmVtYWls"
+                />
+                <input type="hidden" name="name" value="" />
+              </label>
+              <div className="validator-hint hidden">
+                Enter valid email address
+              </div>
+            </div>
+            <button className="btn btn-neutral join-item rounded-r-full">
+              Subscribe
+            </button>
+          </div>
         </form>
       </div>
     </section>
